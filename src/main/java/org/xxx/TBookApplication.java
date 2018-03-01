d50fead387a4c1a1254b87bd8098ad9938c32ab8
895411044@qq.com <GaryLuo@192.168.1.101>
2018-03-01 15:57:09 +0800

    application

--- src/main/java/org/xxx/TBookApplication.java
+++ src/main/java/org/xxx/TBookApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JwtDemoApplication {
+public class TBookApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JwtDemoApplication.class, args);
+        SpringApplication.run(TBookApplication.class, args);
         System.out.println("Hei,TimeBook is ready!--> http://127.0.0.1:8080/");
     }
     
